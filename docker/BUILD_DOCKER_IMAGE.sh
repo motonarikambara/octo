365af1b2a01178105fcb98163b354e9886aebaae
TMats <t.matsu.changmin@gmail.com>
2024-01-08 17:51:51 +0900

    Fix docker for rlds_dataset_mod

--- docker/BUILD_DOCKER_IMAGE.sh
+++ docker/BUILD_DOCKER_IMAGE.sh
@@ -6,4 +6,12 @@ else
   TAG_NAME="latest"
 fi
 
+# Setting up dataset path
+if [ -n "$DATASETS_PATH" ]; then
+  echo "DATASETS_PATH is set to '$DATASETS_PATH'. Mounting this path to the container as '/root/datasets'."
+else
+  export DATASETS_PATH="$PWD/../datasets"
+  echo "DATASETS_PATH is not set. Using default path: $DATASETS_PATH"
+fi
+
 docker-compose -p octo build ${TAG_NAME}
